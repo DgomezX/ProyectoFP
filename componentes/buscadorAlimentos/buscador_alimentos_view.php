5f15b9a0acaffa21109c78a0004c34893eab8fe3
Daniel Gómez Martínez <dgomez10@outlook.es>
2018-06-07 21:05:34 +0200

    Iniciada maquetación input busqueda alimentos

--- componentes/buscadorAlimentos/buscador_alimentos_view.php
+++ componentes/buscadorAlimentos/buscador_alimentos_view.php
@@ -7,6 +7,20 @@
     <?php include 'componentes/commons/header.php'; 	// header ?>
     <?php include 'componentes/commons/menu_nav.php'; 	// menu navegacion ?>
     <h1>Buscador de alimentos</h1>
+    <section id="form_buscador_alim">
+        <div class="container">
+            <div class="row">
+                <div class="col-12">
+                    <div class="box">
+                        <div class="container-1">
+                            <span class="icon"><i class="fa fa-search"></i></span>
+                            <input type="search" id="search" placeholder="Search..." />
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
     <?php include 'componentes/commons/footer.php'; 	// footer ?>
 </body>
 </html>
